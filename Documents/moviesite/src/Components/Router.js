41bd2b65a0a0d3340b45bfc15b497fc24b1844e2
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-03-27 00:23:13 +0900

    0327 - GlobalStyles and Header, CSS stuff in Header.js

--- Documents/moviesite/src/Components/Router.js
+++ Documents/moviesite/src/Components/Router.js
@@ -1,16 +1,25 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import { 
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch 
+} from "react-router-dom";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
+import Header from "Components/Header";
 import Search from "Routes/Search";
 
 export default () => (
   <Router>
-    <Switch>  
-      <Route path="/" exact component={Home} />
-      <Route path="/tv" exact component={TV} />
-      <Route path="/search" exact component={Search} />
-      <Redirect from="*" to="/" />
-    </Switch>
+    <>
+      <Header />
+      <Switch>  
+        <Route path="/" exact component={Home} />
+        <Route path="/tv" exact component={TV} />
+        <Route path="/search" exact component={Search} />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </>
   </Router>
 );
