252656f5b70d63777e26405201afd86fccee6d38
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-03-28 20:57:37 +0900

    #checkpoint

--- Documents/moviesite/src/Routes/TV/TVContainer.js
+++ Documents/moviesite/src/Routes/TV/TVContainer.js
@@ -36,7 +36,6 @@ export default class extends React.Component{
 
   render() {
     const {topRated, popular, airingToday, loading, error} = this.state;
-    console.log(this.state);
     return ( <TVPresenter 
       topRated={topRated}
       popular={popular}
