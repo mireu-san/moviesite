cee070f94a5768714d3a558c46f043a84702963f
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-03-31 21:28:30 +0900

    Loader.js - Ah, Loading screen. Great step for me. Done!

--- Documents/moviesite/src/Routes/Home/HomePresenter.js
+++ Documents/moviesite/src/Routes/Home/HomePresenter.js
@@ -1,15 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-
-import Section from "Components/Section";
+import Loader from "../../Components/Loader";
+import Section from "../../Components/Section";
 
 const Container = styled.div`
   padding: 0px 10px;
 `;
 
 const HomePresenter = ({nowPlaying, upcoming, popular, loading, error}) => 
-  loading ? null : (
+  loading ? (
+  <Loader />
+  ) : (
     <Container>
       {nowPlaying && nowPlaying.length > 0 && (
         <Section title="Now Playing">
