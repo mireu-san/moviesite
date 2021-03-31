36ff81f8efc905775f0759c3cc7b20633db64188
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-03-31 14:05:45 +0900

    fixed typo mistake

--- Documents/moviesite/src/Routes/Home/HomePresenter.js
+++ Documents/moviesite/src/Routes/Home/HomePresenter.js
@@ -2,13 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
-const HomePresenter = ({
-  nowPlaying,
-  upcoming,
-  popular,
-  loading,
-  error,
-}) => null;
+const HomePresenter = ({nowPlaying, upcoming, popular, loading, error}) => null;
 
 HomePresenter.propTypes = {
   nowPlaying: PropTypes.array,
--- Documents/moviesite/src/Routes/Search/SearchPresenter.js
+++ Documents/moviesite/src/Routes/Search/SearchPresenter.js
@@ -2,14 +2,21 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
-const SearchPresenter = ({movieResults, tvResults, loading, searchTerm, handleSubmit, error}) => null;
+const SearchPresenter = ({
+  movieResults, 
+  tvResults, 
+  loading, 
+  searchTerm, 
+  handleSubmit, 
+  error
+}) => null;
 
 SearchPresenter.propTypes = {
   movieResults:PropTypes.array,
   tvResults:PropTypes.array,
   error:PropTypes.string,
   searchTerm:PropTypes.string,
-  loading:PropTypes.boolean.isRequired,
+  loading:PropTypes.bool.isRequired,
   handleSubmit:PropTypes.func.isRequired
 };
 
